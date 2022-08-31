021eb2ccdd0d942e87146091d9133d4e90c6749c
송민진 <m_6595@naver.com>
2022-09-01 06:17:10 +0900

    마이페이지 구현 완료

--- src/main/java/com/example/intermediate/repository/CommentRepository.java
+++ src/main/java/com/example/intermediate/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.example.intermediate.repository;
 
 import com.example.intermediate.domain.Comment;
+import com.example.intermediate.domain.Member;
 import com.example.intermediate.domain.Post;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
   List<Comment> findAllByPost(Post post);
   List<Comment> findAllByPostAndParent(Post post, Comment parent);
+  List<Comment> findAllByMember(Member member);
 }
